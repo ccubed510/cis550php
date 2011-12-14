a46c7e2e8fafab95c5ba47e31f56d2430fd436ce
Annie Lee <annielee89@gmail.com>
2011-12-13 22:05:10 -0500

    More UI updates

--- getuserinfo.php
+++ getuserinfo.php
@@ -3,55 +3,6 @@
 	    <link rel="stylesheet" media="screen" type="text/css" href="css/home.css" />
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 		<script type="text/javascript" src="/js/getuserinfo.js"></script>
-		<style type="text/css">
-			#container {
-			    width: 1000px;
-			    margin:0 auto;
-			    position:relative;
-			}
-			
-			#profile, #feed {
-				padding:15px;
-				position:relative;
-				vertical-align:text-top;
-			}
-			
-			#friendRec, #circle {
-				position:relative;
-				vertical-align:text-top;
-			}
-			
-			#profile {
-				width:40%;
-			}
-			
-			#feed {
-				width:60%;
-			}
-			
-			.header {
-				background: #6F7D94;
-				padding: 0 10px 0 10px;
-				height: 60px;
-			}
-			
-			.profile {
-				background: #A4B2CA;
-			}
-			
-			.feed {
-				background: #BAB0CC;
-			}
-			
-			h1 {
-				font-size: 24pt;
-				color: #ffffff;
-			}
-			
-			h2, h3{
-				font-family: Palatino Linotype;
-			}
-		</style>
 	</head>
 	
 	<title>Profile</title>
@@ -71,18 +22,18 @@
 				<td width="100%" colspan="2" align="center"><? include ('php/getTopPhotos.php'); ?></td>
 			</tr>
 			<tr>
-				<td class="profile" id="profile">
+				<td class="panel" id="profile">
 					<h3>Profile:</h3>
 					<? include ('php/userinfo.php'); ?>
 				</td>
-				<td class="feed" id="feed"><h3>News Feed:</h3>
+				<td class="panel" id="feed"><h3>News Feed:</h3>
 					<? include ('php/newsFeed.php'); ?></td>
 			</tr>
 			<tr>
 				<? include ('php/friendRec.php'); ?>
 			</tr>
 			<tr>
-				<td class="circle" id="circle" colspan="2"><? include ('php/viewCircles.php'); ?><hr></td>
+				<td class="panel" id="circle" colspan="2"><? include ('php/viewCircles.php'); ?></td>
 			</tr>
 			<tr>
 				<td colspan="2">
--- php/friendRec.php
+++ php/friendRec.php
@@ -187,13 +187,13 @@
 	if (mysql_num_rows($test) == 0) {
 		//echo "Error: unable to get temp recommendations.";
 	} else {
-		echo "<td class='friendRec' id='friendRec' colspan='2'><hr><b>Friend Recommendations</b>";
+		echo "<td class='panel' id='friendRec' colspan='2'><b>Friend Recommendations</b>";
 		echo "<table>";
 	while($row = mysql_fetch_array($test)){
 		$circleRec = getCircleRecs($row['UID']);
 		echo "<tr><td><a href=\"/addFriend.php?first=".$row['first']."&last=".$row['last']."&circle=".$circleRec."\">" .$row['first']." ".$row['last'] . "</a></td></tr>";				
 	}
-	echo "</table><hr>";
+	echo "</table>";
 	echo "</td>";
 	}
 	
