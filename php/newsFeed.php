2182647cd6c419c91c7129d7ac00c0e13a7511ef
unknown <Bryan Chiang@BryanChiang-PC.(none)>
2011-12-19 22:07:29 -0500

    Updated news feed to only show visible photos

--- php/newsFeed.php
+++ php/newsFeed.php
@@ -15,7 +15,7 @@
 	$row = mysql_fetch_array($temp);
 	$userID = $row['userID'];
 	
-	$query = "SELECT P.userID AS friendID, P.action AS action, P.objectID AS objectID, P.time AS time FROM Post P, (SELECT DISTINCT F.friendID AS friend FROM Friend F, Circle C WHERE F.circleID = C.circleID AND C.userID = '".$userID."') myFriends WHERE P.userID = myFriends.friend ORDER BY P.time DESC LIMIT 5";
+	$query = "SELECT DISTINCT P.userID AS friendID, P.action AS action, P.objectID AS objectID, P.time AS time FROM Post P, Visible V, Photo PH, (SELECT DISTINCT F.friendID AS friend FROM Friend F, Circle C WHERE F.circleID = C.circleID AND C.userID = '".$userID."') myFriends WHERE P.userID = myFriends.friend AND ((P.objectID = V.photoID AND V.viewerID = '".$userID."') OR (P.objectID = PH.photoID AND PH.Visibility = 'public')) ORDER BY P.time DESC LIMIT 5";
 	$posts = mysql_query($query);
 	
 	
@@ -24,7 +24,6 @@
 		$query = "SELECT first_name, last_name FROM User WHERE userID = '".$row['friendID']."'";
 		$temp = mysql_query($query);
 		$name = mysql_fetch_array($temp);
-		echo "<script type=\"text/javascript\">obj=new Object();</script>";
 		echo "<tr><td>".$name['first_name']." ".$name['last_name']." ".$row['action'] . " photo ".$row['objectID']." (".$row['time'].")</td>";
 		$photo = mysql_query("SELECT url FROM Photo WHERE photoID = '".$row['objectID']."'");
 		$url = mysql_fetch_array($photo);
